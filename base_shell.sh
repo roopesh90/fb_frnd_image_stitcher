65478f124dd0cc32962de7f43f5854a909c3be79
roopesh90 <roopesh90nair@live.com>
2014-08-03 17:30:46 +0530

    Final running bash script Satrting point

--- base_shell.sh
+++ base_shell.sh
@@ -1,2 +1,10 @@
+#Run Me
+clear
 python get_pics.py
+echo -e "\n\nTaking a Breather for 2 secs\n\n"
+sleep 2
+echo -e "\nRunning Stitching script by @swvist"
 python swvist_imagegrid.py
+echo -e "\n\nDone...\n Check for output.jpeg"
+echo -e " \n Thank you"
+
